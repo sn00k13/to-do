067a3f0bf8c42075b282dc4c8b760aed9e1222dd
sn00k13 <gocitek@gmail.com>
2024-06-25 04:04:13 +0100

    more js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,31 @@ const add = document.getElementById("add-btn");
 const addNew = document.getElementById("add-new");
 const addList = document.getElementById("form-button");
 const showList = document.getElementById("list");
+const delList = document.getElementById("del-btn");
+const editList = document.getElementById("edit-btn")
+
+const checkInput = document.createElement("INPUT");
+const textInput = document.createElement("p");
+const node = document.createTextNode("Item 1")
 
 add.addEventListener("click", () => {
     addNew.style.display = "inline-block";
 });
 
 addList.addEventListener("click", () => {
-    showList.style.display = "flex"
+    showList.style.display = "flex";
+    checkInput.style.display = "block"
+    checkInput.setAttribute("type", "checkbox");
+    textInput.appendChild(node);
+    showList.appendChild(checkInput);
+    showList.appendChild(textInput);
+});
+
+delList.addEventListener("click", () => {
+    textInput.remove("p");
+    checkInput.style.display = "none"
+});
+
+editList.addEventListener("click", () => {
+    addNew.style.display = "inline-block";
 });
